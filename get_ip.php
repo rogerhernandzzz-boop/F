d353848d459de1a62e178871615be5c19a7c8367
rogerhernandzzz-boop <rogerhernandzzz@gmail.com>
2026-06-12 12:42:10 -0400

    get_ip.php

--- get_ip.php
+++ get_ip.php
@@ -1,5 +1,4 @@
 <?php
 header('Content-Type: application/json');
-$ip = $_SERVER['REMOTE_ADDR'];
+$ip = $_SERVER['HTTP_CF_CONNECTING_IP'] ?? $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'] ?? '0.0.0.0';
 echo json_encode(['ip' => $ip]);
-?>
